451eea48bd47e806a2327a2c045e73398a8e1090
Danilo Krausz <danilo.krausz@gmail.com>
2023-12-19 12:05:31 +0100

    iniciada a estilizacao

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./styles/index.scss"
+
+function App() {
+
+
+  return (
+    <>
+    <button className="button">botao</button>
+    <button className="button sm">botao sm</button>
+    <button className="button negative">negative</button>
+    <button className="button negative sm">negati sm</button>
+    <button className="button disable"></button>
+    <button className="button disable sm"></button>
+    </>
+  )
+}
+
+export default App
