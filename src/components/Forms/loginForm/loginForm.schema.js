d3ad676ac17d73f888ff0cd672fbf24ea775fd1f
Danilo Krausz <danilo.krausz@gmail.com>
2024-01-04 13:57:23 +0100

    corrigida a entrega

--- src/components/Forms/loginForm/loginForm.schema.js
+++ src/components/Forms/loginForm/loginForm.schema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
-  email: z.email().min(1, "Este campo é obrigatorio"),
+  email: z.string().email("digite um email valido").min(1, "Este campo é obrigatorio"),
   password: z.string().min(1, "Este campo é obrigatorio"),
 });
